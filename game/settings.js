ea64b1715879d9015204d7fe355c36cc814c7946
agusanggarwanto <agusanggarwanto@gmail.com>
2024-06-28 16:54:18 +0700

    intro

--- game/settings.js
+++ game/settings.js
@@ -1,9 +1,10 @@
 "use strict"
 
-settings.title = "Your new game"
-settings.author = "Your name"
+settings.title = "Katai Merah"
+settings.author = "Terajari"
 settings.version = "0.1"
 settings.thanks = []
 settings.warnings = "No warnings have been set for this game."
 settings.playMode = "dev"
+settings.intro = `Tak pernah dalam hidupku menyaksikan yang seperti ini.|Tengah malam tadi, <b><i>Bintang Ristian</i></b>, seorang psikolog yang sedang naik daun berkat konten podcast-nya "Jeda Saja Dulu" yang sering viral, mendatangi kantor polisi dengan pakaian dan tangan berlumuran darah. Hanya dua kata yang dia ucapkan kepada petugas jaga: terjadi <b><i>pembunuhan</i></b>. Semua terdiam. Kini, dia mendekam di salah satu ruang tahanan dengan senyum licik, seolah-olah dia telah memenangkan sesuatu. Ingin rasanya menghancurkan ekspresi licik itu dengan tinjuku.|Masalahnya, catatan kriminalnya sebersih bayi baru lahir. Tak pernah kena tilang, tak pernah melakukan hal-hal kontroversial, dia adalah warga negara yang baik. Di kantong jasnya hanya ditemukan sebuah <b></i>buku catatan>/i></b>. Kantor praktiknya terletak di daerah <b>Cempaka Putih<b/>, dan si orang kaya brengsek ini memiliki dua rumah di daerah <b>Sudirman<b/> dan <b>Tanah Abang</b/>. Hanya itu yang baru kita ketahui sejauh ini.|Jadi, segera pergi ke ruang interogasi, dan bawakan jawabannya padaku!`
 
